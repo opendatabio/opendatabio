2b22f4ca3e0c9f063db2ef79d6198bbce7e3a663
Andre Chalom <andrechalom@gmail.com>
2017-06-08 17:38:19 -0300

    Removing spurious log message

--- app/Http/Middleware/SetRequestLocale.php
+++ app/Http/Middleware/SetRequestLocale.php
@@ -21,7 +21,6 @@ class SetRequestLocale
 	{
 		if (Session::has('applocale') AND array_key_exists(Session::get('applocale'), Config::get('languages'))) {
 			App::setLocale(Session::get('applocale'));
-			Log::info ("MA QUE CARALHO SETTING ". Session::get('applocale'));
 		}
 		else { // This is optional as Laravel will automatically set the fallback language if there is none specified
 			App::setLocale(Config::get('app.fallback_locale'));
