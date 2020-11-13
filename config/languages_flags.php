025684f0088f16ec43e00965551c91a42dcbede1
Alberto Vicentini <vicentini.beto@gmail.com>
2020-11-13 11:05:49 -0400

    Modules update and inclusion of a flag-icon-css package

---
+++ config/languages_flags.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This file is part of the OpenDataBio app.
+ * (c) OpenDataBio development team https://github.com/opendatabio
+ */
+
+return [
+    'en' => 'us',
+    'pt' => 'br',
+];
