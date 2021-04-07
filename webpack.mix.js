652894cf5cdb086e5462ac4c6588198d38c94761
Alberto Vicentini <vicentini.beto@gmail.com>
2021-04-07 07:29:50 -0400

    FONTAWESOME fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,12 +18,8 @@ mix.js([
 	'vendor/andrechalom/laravel-multiselect/resources/assets/js/multiselect.js',
 	'node_modules/spectrum-colorpicker/spectrum.js',
 	], 'public/js')
-   // app css
-<<<<<<< HEAD
-=======
 	 .copy(
 	 			'node_modules/@fortawesome/fontawesome-free/webfonts',
 	 			'public/fonts'
 	  )
->>>>>>> bc12122f19300d4b8117e0f28f84a49229a96a7e
 	 .sass('resources/assets/sass/app.scss', 'public/css');
